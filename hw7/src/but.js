163478875f388249673dd474c7fa81e63fee495f
seafuu <100269487+seafuu@users.noreply.github.com>
2022-05-17 20:17:57 +0800

    Add files via upload

---
+++ hw7/src/but.js
@@ -0,0 +1,8 @@
+
+const button = () => {
+    var output = <div><h1><button class="button" >登入</button></h1></div>
+
+    return output;
+}
+
+export default button;
---
+++ hw7/src/password.js
@@ -0,0 +1,7 @@
+const password = () => {
+    var output = <div>password     <input type="password" class="password"></input></div>
+
+    return output;
+}
+
+export default password;
